f5a78c8a64c4ef2e2679405292cbec3fc854bcf7
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-06-22 14:50:01 -0300

    Best practice useEffect and created authentication hook

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,76 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { auth, firebase } from "../services/firebase";
+
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
+type AuthContextType = {
+  user: User | undefined;
+  signInWithGoogle: () => Promise<void>;
+}
+
+type AuthContextProviderProps = {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextType);
+
+export function AuthContextProvider(props: AuthContextProviderProps) {
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+  // É considerado uma boa prática, sempre que declararmos um Event Listener no React
+  // dentro do useEffect, salvarmos o evento dentro de uma variável para que com essa
+  // variável possamos "desligar" o Event Listener...
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        const { displayName, photoURL, uid } = user;
+
+        if (!displayName || !photoURL) {
+          throw new Error('Missing information from Google Account.');
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL
+        })
+      }
+    })
+  
+  // ... ao final do método useEffect é recomendado fazermos o retorno da função que 
+  // irá descadastrar de todos os Event Listener que houve o cadastramento.
+    return () => {
+      unsubscribe();
+    }
+  }, [])
+
+  async function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    const result = await auth.signInWithPopup(provider);
+
+    if (result.user) {
+      const { displayName, photoURL, uid } = result.user
+
+      if (!displayName || !photoURL) {
+        throw new Error('Missing information from Google Account.');
+      }
+
+      setUser({
+        id: uid,
+        name: displayName,
+        avatar: photoURL
+      })
+    }
+  } 
+
+  return (
+    <AuthContext.Provider value={{ user, signInWithGoogle }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
